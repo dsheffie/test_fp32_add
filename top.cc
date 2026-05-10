ac0f1877866791a395df2f13416f08984b8adc7d
David Sheffield <sheffield.david@gmail.com>
2026-05-10 06:58:59 -0700

    don't need shiftreg - purely combinational

--- top.cc
+++ top.cc
@@ -25,53 +25,51 @@ int main(int argc, char *argv[]) {
   uint32_t x_a = 1, x_b = 2;
   double sum = 0.0;
   size_t t = 0;
-  for(int i = 0; i < 2; i++) {
-    x_a++; x_b++;
-    init_x_a = x_a;
-    init_x_b = x_b;    
-    while( true ) {
-      contextp->timeInc(1);  // 1 timeprecision periodd passes...    
-      tb->clk = 0;
-      tb->eval();
+  x_a++; x_b++;
+  init_x_a = x_a;
+  init_x_b = x_b;    
+  while( true ) {
+    contextp->timeInc(1);  // 1 timeprecision periodd passes...    
+    tb->clk = 0;
+    tb->eval();
       
-      float a = r(x_a);
-      float b = r(x_b);
+    float a = r(x_a);
+    float b = r(x_b);
       
-      if(not(std::isfinite(a) and std::isfinite(b))) {
-	continue;
-      }
-    
-      tb->a = *reinterpret_cast<uint32_t*>(&a);
-      tb->b = *reinterpret_cast<uint32_t*>(&b);    
-      
-      tb->clk = 1;
-      tb->eval();
-      
-      float y = *reinterpret_cast<float*>(&(tb->y));
-      float yy = a + b;
-      double err = (yy-y);
-      err *= err;
-
-      if(std::isfinite(yy)) {
-	sum += yy;
-	t++;
-      }
-      
-      if(err > max_err) {
-	printf("%g + %g = %g, yy = %g\n", a, b, y, yy);
-	max_a = a;
-	max_b = b;
-	max_err = err;
-      }
-    
-      if(x_a == init_x_a) {
-	break;
-      }
+    if(not(std::isfinite(a) and std::isfinite(b))) {
+      continue;
     }
+    
+    tb->a = *reinterpret_cast<uint32_t*>(&a);
+    tb->b = *reinterpret_cast<uint32_t*>(&b);    
+      
+    tb->clk = 1;
+    tb->eval();
+      
+    float y = *reinterpret_cast<float*>(&(tb->y));
+    float yy = a + b;
+    double err = (yy-y);
+    err *= err;
 
-    printf("max error = %g, a = %g, b = %g\n",
-	   max_err, max_a, max_b);
+    if(std::isfinite(yy)) {
+      sum += yy;
+      t++;
+    }
+      
+    if(err > max_err) {
+      printf("%g + %g = %g, yy = %g\n", a, b, y, yy);
+      max_a = a;
+      max_b = b;
+      max_err = err;
+    }
+    
+    if(x_a == init_x_a) {
+      break;
+    }
   }
+
+  printf("max error = %g, a = %g, b = %g\n",
+	 max_err, max_a, max_b);
   printf("avg answer = %g\n", sum / static_cast<double>(t));
   
   delete tb;
