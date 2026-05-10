1a013ebba09b00b24cc7d0fe891fbd8653776fa5
David Sheffield <sheffield.david@gmail.com>
2026-05-10 06:55:34 -0700

    diddle with randomness

--- top.cc
+++ top.cc
@@ -1,5 +1,6 @@
 #include <verilated.h>
 #include <algorithm>
+#include <cmath>
 #include "Vfp_add.h"
 #include "svdpi.h"
 
@@ -19,52 +20,60 @@ int main(int argc, char *argv[]) {
   tb->sub = 0;
   tb->en = 1;
   
-  const uint32_t init_x_a = 1;
-  uint32_t x_a = 1, x_b = 2;
+  uint32_t init_x_a = 1, init_x_b = 2;
   double max_err = 0.0, max_a = 0.0, max_b = 0.0;
-
-  double *errors = new double[1UL<<32];
-  size_t p = 0;
-  
-  while( !Verilated::gotFinish() ) {
-    contextp->timeInc(1);  // 1 timeprecision periodd passes...    
-    tb->clk = 0;
-    tb->eval();
-
-    float a = r(x_a);
-    float b = r(x_b);
+  uint32_t x_a = 1, x_b = 2;
+  double sum = 0.0;
+  size_t t = 0;
+  for(int i = 0; i < 2; i++) {
+    x_a++; x_b++;
+    init_x_a = x_a;
+    init_x_b = x_b;    
+    while( true ) {
+      contextp->timeInc(1);  // 1 timeprecision periodd passes...    
+      tb->clk = 0;
+      tb->eval();
+      
+      float a = r(x_a);
+      float b = r(x_b);
+      
+      if(not(std::isfinite(a) and std::isfinite(b))) {
+	continue;
+      }
     
-    tb->a = *reinterpret_cast<uint32_t*>(&a);
-    tb->b = *reinterpret_cast<uint32_t*>(&b);    
+      tb->a = *reinterpret_cast<uint32_t*>(&a);
+      tb->b = *reinterpret_cast<uint32_t*>(&b);    
+      
+      tb->clk = 1;
+      tb->eval();
+      
+      float y = *reinterpret_cast<float*>(&(tb->y));
+      float yy = a + b;
+      double err = (yy-y);
+      err *= err;
+
+      if(std::isfinite(yy)) {
+	sum += yy;
+	t++;
+      }
+      
+      if(err > max_err) {
+	printf("%g + %g = %g, yy = %g\n", a, b, y, yy);
+	max_a = a;
+	max_b = b;
+	max_err = err;
+      }
     
-    tb->clk = 1;
-    tb->eval();
-
-    float y = *reinterpret_cast<float*>(&(tb->y));
-    float yy = a + b;
-    double err = (yy-y);
-    err *= err;
-
-    if(err > max_err) {
-      printf("%g + %g = %g, yy = %g\n", a, b, y, yy);
-      max_a = a;
-      max_b = b;
-      max_err = err;
+      if(x_a == init_x_a) {
+	break;
+      }
     }
-    errors[p++] = err;
-    
-    if(x_a == init_x_a) {
-      break;
-    }
+
+    printf("max error = %g, a = %g, b = %g\n",
+	   max_err, max_a, max_b);
   }
-  std::sort(errors, errors + p);
-
-  printf("max error = %g, a = %g, b = %g, p = %zu\n",
-	 max_err, max_a, max_b, p);
-
-  printf("median error = %g\n", errors[p/2]);
+  printf("avg answer = %g\n", sum / static_cast<double>(t));
   
   delete tb;
-  delete [] errors;
   return 0;
 }
